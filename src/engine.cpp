585a54ddac1bf6410714dddbc97df0f9e7c05779
yams <yams@yams.yams>
2015-06-05 03:11:19 +0000

    brahhh

--- src/engine.cpp
+++ src/engine.cpp
@@ -262,7 +262,6 @@ namespace Sb {
                                           ++it;
                                     }
                               }
-
                         }
                   }
             }
